fbf3f1c328163bb627316f9dfab7ba44b592c01b
neville <kilonzoneville@gmail.com>
2018-07-30 09:10:04 +0300

    add  the model for the user Profile

--- gram/models.py
+++ gram/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save,post_delete
+from django.dispatch import receiver
+import datetime as dt
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=30)
+    user_name = models.CharField(max_length=30)
+    prof_pic = models.ImageField(upload_to= 'profiles/', blank=True,default="https://drawinglics.com/view/3763785/prosthetic-knowledge-wouldnt-it-be-funny-if-every-facebook-wouldnt-it-be-funny-if-every-facebook-profile-pic-was-this-via.jpg")
+    bio = models.TextField(max_length=50, blank=True)
