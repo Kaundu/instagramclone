f76a571b40cc1a6c48ef48363c8a1347a81ad9a2
neville <kilonzoneville@gmail.com>
2018-08-01 10:13:23 +0300

    add the app urls for the new post page

--- gram/urls.py
+++ gram/urls.py
@@ -4,4 +4,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns=[
-    url('^$',views.index,name = 'index'),
+    url('^$',views.index,name = 'index'),    url('^$',views.index,name = 'index'),
+    url(r'^new/post$', views.new_post, name='new-post'),    url('^$',views.index,name = 'index'),
+    url(r'^new/post$', views.new_post, name='new-post'),
