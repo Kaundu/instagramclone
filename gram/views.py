5b61496176aa6a4ce97fc8cdb60f58bbf5240ab0
neville <kilonzoneville@gmail.com>
2018-08-01 10:39:52 +0300

    add the view function to allow one to add a post

--- gram/views.py
+++ gram/views.py
@@ -14,6 +14,21 @@ def index(request):
 
 
 @login_required(login_url='/accounts/login/')
+def new_post(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = NewPostForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.user = current_user
+            post.save()
+            return redirect('index')
+    else:
+        form = NewPostForm()
+    return render(request, 'new_post.html', {"form":form})
+
+@login_required(login_url='/accounts/login/')
 def profile(request,user_id=None):
     if user_id == None:
         user_id=request.user.id
