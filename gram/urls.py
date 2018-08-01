dfe1083356c683b9e23abd09f388036d446b0c7a
neville <kilonzoneville@gmail.com>
2018-08-01 10:13:40 +0300

    add the app urls for the profile page

--- gram/urls.py
+++ gram/urls.py
@@ -6,4 +6,4 @@ from django.conf.urls.static import static
 urlpatterns=[
     url('^$',views.index,name = 'index'),    url('^$',views.index,name = 'index'),
     url(r'^new/post$', views.new_post, name='new-post'),    url('^$',views.index,name = 'index'),
-    url(r'^new/post$', views.new_post, name='new-post'),
+    url(r'^new/post$', views.new_post, name='new-post'),    url(r'^profile/(?P<user_id>\d+)?$', views.profile, name='profile'),
