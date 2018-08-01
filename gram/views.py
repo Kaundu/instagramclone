cb1e9441147d118dda87855db0fb011ee2aa09d7
neville <kilonzoneville@gmail.com>
2018-08-01 10:12:00 +0300

    add the view functions for the profile page

--- gram/views.py
+++ gram/views.py
@@ -12,5 +12,17 @@ def index(request):
     return render(request, 'index.html', locals())
 
 
+@login_required(login_url='/accounts/login/')
+def profile(request,user_id=None):
+    if user_id == None:
+        user_id=request.user.id
+    current_user = User.objects.get(id = user_id)
+    # user = current_user
+    images = Post.objects.filter(user=current_user)
+    profile = Profile.objects.all()
+    followers = len(Follow.objects.followers(current_user))
+    following = len(Follow.objects.following(current_user))
+    return render(request, 'profile.html', locals())
+
 
 # Create your views here.
