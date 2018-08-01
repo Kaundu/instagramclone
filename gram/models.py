2d45b693be55a9fb56c9b44b5ba62e5b81937680
neville <kilonzoneville@gmail.com>
2018-08-01 10:10:37 +0300

    update the input for the models

--- gram/models.py
+++ gram/models.py
@@ -7,9 +7,9 @@ import datetime as dt
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=30)
-    user_name = models.CharField(max_length=30)
-    prof_pic = models.ImageField(upload_to= 'profiles/', blank=True,default="https://drawinglics.com/view/3763785/prosthetic-knowledge-wouldnt-it-be-funny-if-every-facebook-wouldnt-it-be-funny-if-every-facebook-profile-pic-was-this-via.jpg")
+    name = models.CharField(max_length=30, blank=True)
+    user_name = models.CharField(max_length=30,blank=True)
+    prof_pic = models.ImageField(upload_to= 'profiles/', blank=True,default="profile/a.jpg")
     bio = models.TextField(max_length=50, blank=True)
 
 
