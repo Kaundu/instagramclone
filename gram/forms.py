9e64c709baed1ccafc7a4e5d554bf4b22a90f56e
neville <kilonzoneville@gmail.com>
2018-08-01 10:30:48 +0300

    add the form class for the update profile form

--- gram/forms.py
+++ gram/forms.py
@@ -5,3 +5,14 @@ class NewPostForm(forms.ModelForm):
     class Meta :
         model = Post
         exclude = ['user', 'post_date','liker']
+
+class UserForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ('name','user_name','bio')
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        exclude = ['user']
+
